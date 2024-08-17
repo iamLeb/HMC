078ddaf6a57234a2222f96fbb00a68f83744ed6b
Caleb Joseph <calebbjoseph@gmail.com>
2024-08-16 21:22:30 -0500

    made services routing from one source

--- src/components/frontComponents/Help.jsx
+++ src/components/frontComponents/Help.jsx
@@ -1,53 +1,7 @@
 import { CiCircleCheck } from "react-icons/ci";
 import { GoArrowRight } from "react-icons/go";
+import {services} from "./ServiceLists.js";
 
-const services = [
-    {
-        title: 'Home Support Services',
-        brief: 'Friendly check-in visits, errands, shopping, planning and preparing meals, housekeeping, laundry, accompaniment to appointments, and incidental transportation. ',
-        list: [
-            "Light housekeeping & Laundry",
-            "Meal Preparation & Planning",
-            "Companionship and Community outings",
-            "Light yard work",
-            "Grocery shopping",
-            "Errands",
-            "Pet care",
-            "Incidental Transportation",
-            "Cleaning out cupboards",
-        ]
-    },
-    {
-        title: 'Personal Care Services',
-        brief: 'Personal Support Workers provide assistance with the private activities of daily living such as hygiene, grooming, toileting, medication reminders, and mobility.  ',
-        list: [
-            "Dressing",
-            "Bathing",
-            "Grooming",
-            "Mobility",
-            "Toileting",
-            "Continence assistance",
-            "Medication Reminders",
-            "Alzheimer & Dementia support",
-            "Respite Care",
-            "Overnight Care"
-        ]
-    },
-    {
-        title: 'Complex Care Services',
-        brief: 'Complex care refers to services that must be performed by a regulated health professional, such as a Registered Nurse or a Registered Practical Nurse. \n' +
-            'These services include:\n',
-        list: [
-            "Medication administration",
-            "Vital signs monitoring",
-            "Wound care",
-            "Catheter care",
-            "Foot care",
-            "Ostomy care",
-            "Palliative Care"
-        ]
-    }
-];
 const Service = () => {
     return (
         <section className={'flex flex-col gap-4 bg-[#f7f5ec] py-5'}>
@@ -57,8 +11,11 @@ const Service = () => {
             </div>
 
             <div className={'container mx-auto md:flex justify-between items-center md:space-x-4 space-y-4 md:space-y-0 p-2'}>
-                { services.map((service, index) => (
-                    <div key={index} className={'flex flex-col gap-4 bg-primary text-white rounded-lg w-full p-8'}>
+                {services.map((service, index) => (
+                    <div key={index} className={'relative flex flex-col gap-4 bg-primary text-white rounded-lg w-full p-8'}>
+                        <div className={'absolute bg-white text-primary p-5 font-san font-extrabold text-xl shadow-lg top-0 right-0 rounded-bl-full'}>
+                            ${service.price}/hr
+                        </div>
                         <h1 className={'font-bold text-2xl'}>{service.title}</h1>
                         <p>{service.brief}</p>
                         <ul className={'flex flex-col gap-4 mt-5'}>
--- src/pages/front/Service.jsx
+++ src/pages/front/Service.jsx
@@ -1,57 +1,8 @@
 import SubHeader from "../../components/frontComponents/SubHeader.jsx";
 import {CiCircleCheck} from "react-icons/ci";
 import {GoArrowRight} from "react-icons/go";
+import {services} from "../../components/frontComponents/ServiceLists.js";
 
-const services = [
-    {
-        title: 'Home Support Services',
-        price: '25',
-        brief: 'Friendly check-in visits, errands, shopping, planning and preparing meals, housekeeping, laundry, accompaniment to appointments, and incidental transportation. ',
-        list: [
-            "Light housekeeping & Laundry",
-            "Meal Preparation & Planning",
-            "Companionship and Community outings",
-            "Light yard work",
-            "Grocery shopping",
-            "Errands",
-            "Pet care",
-            "Incidental Transportation",
-            "Cleaning out cupboards",
-        ]
-    },
-    {
-        title: 'Personal Care Services',
-        price: '28',
-        brief: 'Personal Support Workers provide assistance with the private activities of daily living such as hygiene, grooming, toileting, medication reminders, and mobility.  ',
-        list: [
-            "Dressing",
-            "Bathing",
-            "Grooming",
-            "Mobility",
-            "Toileting",
-            "Continence assistance",
-            "Medication Reminders",
-            "Alzheimer & Dementia support",
-            "Respite Care",
-            "Overnight Care"
-        ]
-    },
-    {
-        title: 'Complex Care Services',
-        price: '35',
-        brief: 'Complex care refers to services that must be performed by a regulated health professional, such as a Registered Nurse or a Registered Practical Nurse. \n' +
-            'These services include:\n',
-        list: [
-            "Medication administration",
-            "Vital signs monitoring",
-            "Wound care",
-            "Catheter care",
-            "Foot care",
-            "Ostomy care",
-            "Palliative Care"
-        ]
-    }
-];
 const Service = () => {
     return (
         <section>
